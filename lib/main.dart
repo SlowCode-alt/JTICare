98048ca329ff5f31a8d69b8beb4d7b0f9f0d12f1
Ardy Sofyan <ardyrolasi@gmail.com>
2025-05-30 05:59:57 +0700

    conflict lagi, ilangin bar hitam lagi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // lib/main.dart
+import 'package:flutter/services.dart';  // Add this import
 import 'package:flutter/material.dart';
 import 'package:project_akhir_donasi_android/login.dart';
 import 'package:project_akhir_donasi_android/dashboard/dashboard.dart';
@@ -8,6 +9,7 @@ import 'package:shared_preferences/shared_preferences.dart'; // <<<--- Import in
 import 'package:project_akhir_donasi_android/widget/intro_splashscreen.dart'; // <<<--- Import IntroSplashScreen Anda
 
 void main() async {
+   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
@@ -72,8 +74,7 @@ class _SplashScreenState extends State<SplashScreen> {
     final token = await TokenManager.getToken();
     // Beri delay tambahan jika ingin splash screen awal (logo) tampil lebih lama
     await Future.delayed(
-        const Duration(seconds: 3)); // Delay tambahan untuk logo splash
-
+         const Duration(seconds: 3)); // Delay tambahan untuk logo splash
     if (!mounted) return;
 
     if (token != null && token.isNotEmpty) {
